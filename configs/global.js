c1e6c98201633d14a6e5858168a35095e7ad0c90
Oladejo Jephthah <jephthahooh@gmail.com>
2025-05-17 19:02:38 +0100

    structure converts from singular from plural

---
+++ configs/global.js
@@ -0,0 +1,23 @@
+// Declaring Global variables
+
+const path = require('path');
+
+module.exports = function () {
+    global.__basedir = __dirname.replace('/configs', ''); // Adjust based on actual path
+    global.CONFIGS = path.join(__basedir, 'configs/');
+    global.DTOS = path.join(__basedir, 'dtos/');
+    global.UTILS = path.join(__basedir, 'utils/');
+    global.CONTROLLERS = path.join(__basedir, 'controllers/');
+    global.ROUTES = path.join(__basedir, 'routes/');
+    global.MODELS = path.join(__basedir, 'models/');
+    global.SERVICES = path.join(__basedir, 'services/');
+    global.REPOSITORIES = path.join(__basedir, 'repositories/');
+    global.VENDORS = path.join(__basedir, 'vendor/');
+    global.MIDDLEWARE = path.join(__basedir, 'middleware/');
+    global.RESOURCE = path.join(__basedir, 'resources/');
+
+
+    global.CORES = path.join(UTILS, 'cores/');
+    global.VALIDATORS = path.join(UTILS, 'validators/');
+    global.MAIN_UTILS = path.join(UTILS, 'mains/');
+};
