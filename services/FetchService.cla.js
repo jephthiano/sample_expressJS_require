fc21463fc7fca68fa183db8f28a1ad99c62087eb
Oladejo Jephthah <jephthahooh@gmail.com>
2025-05-28 08:33:08 +0100

    Fetch Completed

--- services/FetchService.cla.js
+++ services/FetchService.cla.js
@@ -1,15 +1,16 @@
 const BaseService = require(SERVICES + 'BaseService.cla');
 const FetchRepository = require(REPOSITORIES + 'FetchRepository.cla');
 const { setToken } = require(MAIN_UTILS + 'token.util');
+const { selEncrypt }  = require(MAIN_UTILS + 'security.util');
 const UserResource = require(RESOURCES + 'UserResource');
 
 
 class FetchService extends BaseService{
-    static async authFetchData (res, id){
+    static async authFetchData (res, user){
         try {
             //get user data
-            const user = await FetchRepository.getUserById(res, id);
-            const token = user ? setToken(id) : null ;
+            // const user = await FetchRepository.getUserById(res, id);
+            const token = user ? setToken(user.id) : null ;
 
             if(token && user){
                 const data = new UserResource(user).toJSON();
@@ -22,10 +23,11 @@ class FetchService extends BaseService{
         return {};
     }
 
-    static async appFetchData (res, id, token){
+    static async appFetchData (req, res){
         try{
             //get user data
-            const user = await FetchRepository.getUserById(res, id);
+            const user = await FetchRepository.getUserById(res, req.user.id);
+            const token = selEncrypt(req.token, 'token');
 
             if(token && user){
                 const data = new UserResource(user).toJSON();
@@ -33,7 +35,7 @@ class FetchService extends BaseService{
                 return this.sendResponse(res, response, "Success");
             }
         }catch(err){
-            this.handleException(res, error);
+            this.handleException(res, err);
         }
 
         return this.triggerError("User not found", [], 404);
