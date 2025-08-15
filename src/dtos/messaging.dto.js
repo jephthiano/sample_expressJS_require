a080176e5d70b47d783fee3ed91aab2f979fada7
Oladejo Jephthah <jephthahooh@gmail.com>
2025-08-15 08:31:26 +0100

    messaging dto type rename to prevent conflicts

--- src/dtos/messaging.dto.js
+++ src/dtos/messaging.dto.js
@@ -89,7 +89,7 @@ const htmlEmailTemplate = (data) => {
         <head>
             <meta charset='UTF-8'>
             <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-            <meta http-equiv='Content-message_type' content='text/html; charset=UTF-8'>
+            <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
             <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
             <style>
